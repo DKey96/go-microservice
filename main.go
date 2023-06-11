b13e2ccffd81a048631a1d944ef4d59db9f8707a
Daniel Klic <daniel.klic96@gmail.com>
2023-06-11 22:45:22 +0200

    Move server to it's own package

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"crypto/tls"
 	"log"
+	"microservice-in-30-mins/server"
 	"net/http"
 	"os"
-	"time"
 )
 
 const message = "Hello from the other side"
@@ -25,41 +24,11 @@ func main() {
 		w.Write([]byte(message))
 	})
 
-	srv := newServer(mux, ServiceAddr)
+	srv := server.Server(mux, ServiceAddr)
 
 	// If there was an error when starting a server, it is being returned
 	err := srv.ListenAndServeTLS(CertFile, KeyFile)
 	if err != nil {
 		log.Fatalf("Server failed to start. Reason: %v", err)
 	}
-}
-
-func newServer(mux *http.ServeMux, serverAddr string) *http.Server {
-	// Setup based on https://blog.cloudflare.com/exposing-go-on-the-internet/
-	tlsConfig := &tls.Config{
-		// Only use curves which have assembly implementations
-		CurvePreferences: []tls.CurveID{
-			tls.CurveP256,
-			tls.X25519, // Go 1.8 only
-		},
-		MinVersion: tls.VersionTLS12,
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-		},
-	}
-	// Create custom server with TLS Custom Setup and timeouts
-	srv := &http.Server{
-		Addr:         serverAddr,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		IdleTimeout:  120 * time.Second,
-		TLSConfig:    tlsConfig,
-		Handler:      mux,
-	}
-	return srv
 }
---
+++ server/server.go
@@ -0,0 +1,37 @@
+package server
+
+import (
+	"crypto/tls"
+	"net/http"
+	"time"
+)
+
+func Server(mux *http.ServeMux, serverAddr string) *http.Server {
+	// Setup based on https://blog.cloudflare.com/exposing-go-on-the-internet/
+	tlsConfig := &tls.Config{
+		// Only use curves which have assembly implementations
+		CurvePreferences: []tls.CurveID{
+			tls.CurveP256,
+			tls.X25519, // Go 1.8 only
+		},
+		MinVersion: tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		},
+	}
+	// Create custom server with TLS Custom Setup and timeouts
+	srv := &http.Server{
+		Addr:         serverAddr,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		TLSConfig:    tlsConfig,
+		Handler:      mux,
+	}
+	return srv
+}
